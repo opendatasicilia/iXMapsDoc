e8903a0381173f541c73c672e1d2448197d49019
gbvitrano <gbvitrano@gmail.com>
2018-11-24 11:30:15 +0100

    favicon

--- conf.py
+++ conf.py
@@ -102,7 +102,7 @@ html_show_copyright = True
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-html_favicon = "img/favicon_ods_ixmaps.ico"
+html_favicon = "img/favicon.ico"
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
