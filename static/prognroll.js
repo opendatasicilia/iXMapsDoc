ce706312257af12e98788d50b069fdd0851a24c2
gbvitrano <gbvitrano@gmail.com>
2019-01-25 08:21:40 +0100

    upgrade

--- static/prognroll.js
+++ static/prognroll.js
@@ -21,7 +21,7 @@
 
             $span.css({
                 position: "fixed",
-                top: 50,
+                top: 0,
                 left: 0,
                 width: 0,
                 height: settings.height,
